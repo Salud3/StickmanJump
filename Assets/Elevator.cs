e9982a85e85018bc55970541746651cb96545992
Salud3 <78719641+Salud3@users.noreply.github.com>
2023-11-13 12:55:30 -0600

    elevator

---
+++ Assets/Elevator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    public float range;
+    public float speed;
+    public enum ElevatorType { Arriba, Derecha, Enfrente, izquierda };
+    public ElevatorType movimiento;
+    Vector3 initialPosition;
+
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+
+    void Update()
+    {
+        if (movimiento == ElevatorType.Arriba)
+        {
+            transform.position = new Vector3(initialPosition.x, initialPosition.y + Mathf.Sin(Time.fixedTime * speed) * range, initialPosition.z);
+        }
+        if (movimiento == ElevatorType.Derecha)
+        {
+            transform.position = new Vector3(initialPosition.x, initialPosition.y, initialPosition.z + Mathf.Sin(Time.fixedTime * speed) * range);
+        }
+        if (movimiento == ElevatorType.izquierda)
+        {
+            transform.position = new Vector3(initialPosition.x, initialPosition.y, initialPosition.z - Mathf.Sin(Time.fixedTime * speed) * range);  
+        }
+        if (movimiento == ElevatorType.Enfrente)
+        {
+            transform.position = new Vector3(initialPosition.x + Mathf.Sin(Time.fixedTime * speed) * range, initialPosition.y, initialPosition.z);
+        }
+    }
+}
---
+++ Assets/PlayerPlatformInteraction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPlatformInteraction : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="platform")
+        {
+            gameObject.transform.SetParent(other.gameObject.transform);
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "platform")
+        {
+            gameObject.transform.SetParent(null);
+        }
+    }
+
+}
